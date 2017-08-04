364e97cdfd00f1dc4baf30cfa044c0c7040d8495
ulinkwo <ulinkwo@gmail.com>
2017-08-04 10:57:21 +0800

    增加临时二维码场景参数为字符串

--- weixin4j-base/src/main/java/com/foxinmy/weixin4j/type/QRType.java
+++ weixin4j-base/src/main/java/com/foxinmy/weixin4j/type/QRType.java
@@ -2,32 +2,36 @@ package com.foxinmy.weixin4j.type;
 
 /**
  * 二维码类型
- * 
- * @className QRType
+ *
  * @author jinyu(foxinmy@gmail.com)
+ * @className QRType
  * @date 2014年11月4日
- * @since JDK 1.6
  * @see
+ * @since JDK 1.6
  */
 public enum QRType {
-	/**
-	 * 临时二维码
-	 */
-	QR_SCENE,
-	/**
-	 * 永久二维码(场景值为数字范围在1-100000之间)
-	 */
-	QR_LIMIT_SCENE,
-	/**
-	 * 永久二维码(场景值为字符串长度在1-64之间)
-	 */
-	QR_LIMIT_STR_SCENE,
-	/**
-	 * 卡券二维码：单个卡券
-	 */
-	QR_CARD,
-	/**
-	 * 卡券二维码：多个卡券
-	 */
-	QR_MULTIPLE_CARD;
+    /**
+     * 临时二维码(场景值为数字范围在1-100000之间)
+     */
+    QR_SCENE,
+    /**
+     * 临时二维码(场景值为数字范围在1-100000之间)
+     */
+    QR_STR_SCENE,
+    /**
+     * 永久二维码(场景值为数字范围在1-100000之间)
+     */
+    QR_LIMIT_SCENE,
+    /**
+     * 永久二维码(场景值为数字范围在1-100000之间)
+     */
+    QR_LIMIT_STR_SCENE,
+    /**
+     * 卡券二维码：单个卡券
+     */
+    QR_CARD,
+    /**
+     * 卡券二维码：多个卡券
+     */
+    QR_MULTIPLE_CARD
 }
